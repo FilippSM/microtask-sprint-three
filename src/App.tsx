1aa632549c3f2febda729bc57529b22bf48baa00
Filipp Sam <philippsm666@gmail.com>
2025-02-01 01:10:22 +0300

    (sprint 3 week 1) Navlink, Link, a

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from "./components/Site.module.css";
 import { PageOne } from './components/pages/PageOne';
 import { PageTwo } from './components/pages/PageTwo';
 import { PageThree } from './components/pages/PageThree';
-import { Route, Routes } from 'react-router-dom';
-
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { Error404 } from './components/pages/Error404';
 
 function App() {
     return (
@@ -12,25 +12,51 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <div>
+                        <NavLink
+                            to="/page1"
+                            className={({ isActive }) =>
+                                isActive ? styles.activeNavLink : styles.navLink
+                            }
+                        >
+                            Page 1
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink
+                            to="/page2"
+                            className={({ isActive }) =>
+                                isActive ? styles.activeNavLink : styles.navLink
+                            }
+                        >
+                            Page 2
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink
+                            to="/page3"
+                            className={({ isActive }) =>
+                                isActive ? styles.activeNavLink : styles.navLink
+                            }
+                        >
+                            Page 3
+                        </NavLink>
+                    </div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
-                        <Route/>
-                        <Route path="/page1" element={<PageOne />}/>
-                        <Route path="/page2" element={<PageTwo />}/>
-                        <Route path="/page3" element={<PageThree />}/>
-                        
-                    {/*     <PageTwo />
-                        <PageThree /> */}
+                        <Route path="/" element={<Navigate to="/page1" />} />
+                        <Route path="/page1" element={<PageOne />} />
+                        <Route path="/page2" element={<PageTwo />} />
+                        <Route path="/page3" element={<PageThree />} />
+                        <Route path="/error404" element={<Error404 />} />
+                        <Route path="/*" element={<Navigate to="/error404" />} />
                     </Routes>
-
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
         </div>
     );
 }
-
 
 export default App;
